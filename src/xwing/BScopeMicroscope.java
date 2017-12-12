f9c8c4678a68afbd442057c6e3cc2ebe10c0a607
myers-pc-1\BScope <rhaase@mpi-cbg.de>
2017-12-12 14:47:22 +0100

    first commit

---
+++ src/xwing/BScopeMicroscope.java
@@ -0,0 +1,165 @@
+package xwing;
+
+import clearcl.ClearCLContext;
+import clearcontrol.devices.cameras.StackCameraDeviceInterface;
+import clearcontrol.devices.cameras.devices.hamamatsu.HamStackCamera;
+import clearcontrol.devices.lasers.devices.cobolt.CoboltLaserDevice;
+import clearcontrol.devices.lasers.devices.omicron.OmicronLaserDevice;
+import clearcontrol.devices.signalamp.devices.srs.SIM900MainframeDevice;
+import clearcontrol.devices.signalamp.devices.srs.SIM983ScalingAmplifierDevice;
+import clearcontrol.devices.signalgen.devices.nirio.NIRIOSignalGenerator;
+import clearcontrol.devices.stages.StageType;
+import clearcontrol.devices.stages.devices.tst.TSTStageDevice;
+import clearcontrol.devices.stages.hub.StageHubDevice;
+import clearcontrol.microscope.lightsheet.component.detection.DetectionArm;
+import clearcontrol.microscope.lightsheet.component.lightsheet.LightSheet;
+import clearcontrol.microscope.lightsheet.component.opticalswitch.LightSheetOpticalSwitch;
+import clearcontrol.microscope.lightsheet.signalgen.LightSheetSignalGeneratorDevice;
+import clearcontrol.microscope.lightsheet.simulation.SimulatedLightSheetMicroscope;
+
+/**
+ * XWing microscope
+ *
+ * @author royer
+ */
+public class BScopeMicroscope extends SimulatedLightSheetMicroscope
+{
+
+  /**
+   * Instantiates an XWing microscope
+   * 
+   * @param pStackFusionContext
+   *          ClearCL context for stack fusion
+   * @param pMaxStackProcessingQueueLength
+   *          max stack processing queue length
+   * @param pThreadPoolSize
+   *          thread pool size
+   */
+  public BScopeMicroscope(ClearCLContext pStackFusionContext,
+                         int pMaxStackProcessingQueueLength,
+                         int pThreadPoolSize)
+  {
+    super("XWing",
+          pStackFusionContext,
+          pMaxStackProcessingQueueLength,
+          pThreadPoolSize);
+  }
+
+  /**
+   * Assembles the microscope
+   * 
+   * @param pNumberOfDetectionArms
+   *          number of detection arms
+   * @param pNumberOfLightSheets
+   *          number of lightsheets
+   */
+  public void addRealHardwareDevices(int pNumberOfDetectionArms,
+                                     int pNumberOfLightSheets)
+  {
+    long lDefaultStackWidth = 1024;
+    long lDefaultStackHeight = 2048;
+
+    // Setting up lasers:
+    {
+    	
+      final OmicronLaserDevice lLaserDevice488 =
+                                               new OmicronLaserDevice(2);
+      addDevice(0, lLaserDevice488);
+
+      /*final CoboltLaserDevice lLaserDevice594 =
+                                              new CoboltLaserDevice("Mambo",
+                                                                    100,
+                                                                    1);
+      addDevice(1, lLaserDevice594);/**/
+
+    }
+
+    
+    // Setting up cameras:
+    if (true)
+    {
+      for (int c = 0; c < pNumberOfDetectionArms; c++)
+      {
+        StackCameraDeviceInterface<?> lCamera =
+                                              HamStackCamera.buildWithExternalTriggering(c);
+
+        lCamera.getStackWidthVariable().set(lDefaultStackWidth);
+        lCamera.getStackHeightVariable().set(lDefaultStackHeight);
+        lCamera.getExposureInSecondsVariable().set(0.010);
+
+        // lCamera.getStackVariable().addSetListener((o,n)->
+        // {System.out.println("camera output:"+n);} );
+
+        addDevice(c, lCamera);
+      }
+    }
+
+   
+    // Adding signal Generator:
+    LightSheetSignalGeneratorDevice lLSSignalGenerator;
+    {
+      NIRIOSignalGenerator lNIRIOSignalGenerator =
+                                                 new NIRIOSignalGenerator();
+      lLSSignalGenerator =
+                         LightSheetSignalGeneratorDevice.wrap(lNIRIOSignalGenerator,
+                                                              true);
+      // addDevice(0, lNIRIOSignalGenerator);
+      addDevice(0, lLSSignalGenerator);
+    }
+
+    // Setting up detection arms:
+    {
+      for (int c = 0; c < pNumberOfDetectionArms; c++)
+      {
+        final DetectionArm lDetectionArm = new DetectionArm("D" + c);
+        lDetectionArm.getPixelSizeInMicrometerVariable().set(0.26);
+
+        addDevice(c, lDetectionArm);
+      }
+    }
+
+    // Setting up lightsheets:
+    {
+
+      for (int l = 0; l < pNumberOfLightSheets; l++)
+      {
+        final LightSheet lLightSheet =
+                                     new LightSheet("I" + l,
+                                                    9.4,
+                                                    getNumberOfLaserLines());
+        addDevice(l, lLightSheet);
+      }
+    }
+
+    // syncing exposure between cameras and lightsheets, as well as camera image
+    // height:
+    {
+      for (int l = 0; l < pNumberOfLightSheets; l++)
+        for (int c = 0; c < pNumberOfDetectionArms; c++)
+        {
+          StackCameraDeviceInterface<?> lCamera =
+                                                getDevice(StackCameraDeviceInterface.class,
+                                                          c);
+          LightSheet lLightSheet = getDevice(LightSheet.class, l);
+
+          lCamera.getExposureInSecondsVariable()
+                 .sendUpdatesTo(lLightSheet.getEffectiveExposureInSecondsVariable());
+
+          lCamera.getStackHeightVariable()
+                 .sendUpdatesTo(lLightSheet.getImageHeightVariable());
+
+        }
+    }
+
+    // Setting up lightsheets selector
+    {
+      LightSheetOpticalSwitch lLightSheetOpticalSwitch =
+                                                       new LightSheetOpticalSwitch("OpticalSwitch",
+                                                                                   pNumberOfLightSheets);
+
+      addDevice(0, lLightSheetOpticalSwitch);
+    }
+
+  }
+
+}
